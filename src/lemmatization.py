1455f34967a532fe1593a08d83040d685ee38a95
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-06-12 12:33:28 -0500

    Lemmatization using WordNet

--- src/lemmatization.py
+++ src/lemmatization.py
@@ -10,14 +10,14 @@ def main():
             normalized_text = open(os.path.join(dirpath, filename), encoding='utf-8')
             tokens = sorted(list(set(nltk.word_tokenize(normalized_text.read()))))
 
-            processed_tokens = stem(tokens)
+            processed_tokens = lemmatize(tokens)
 
             write_processed_tokens(processed_tokens, dirpath, filename)
 
 
 def write_processed_tokens(tokens, dirpath, filename):
     dirname = re.findall(r'^\.\./data/tokenized/processed/(.*)', dirpath)
-    directory = os.path.join('../data/stemmed/', dirname[0])
+    directory = os.path.join('../data/lemmatized/', dirname[0])
     if not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -27,14 +27,14 @@ def write_processed_tokens(tokens, dirpath, filename):
         tokens_file.write('\n')
 
 
-def stem(tokens):
-    stemmer = nltk.stem.snowball.SpanishStemmer()
-    processed_tokens = sorted(list(set([stemmer.stem(t) for t in tokens])))
+def lemmatize(tokens):
+    lemmatizer = nltk.WordNetLemmatizer()
+    processed_tokens = sorted(list(set([lemmatizer.lemmatize(t) for t in tokens])))
     for t in tokens:
-        stem = stemmer.stem(t)
-        if stem != t:
+        lemma = lemmatizer.lemmatize(t)
+        if lemma != t:
             # Check
-            print("word : ", t, "    stem :", stem)
+            print("word : ", t, "    lemma :", lemma)
     return processed_tokens
 
 
