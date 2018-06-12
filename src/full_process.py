adc6a499a29b537e75fd8e18434de8e016cca304
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-06-12 12:08:33 -0500

    Adding a module which run all the process

---
+++ src/full_process.py
@@ -0,0 +1,13 @@
+import src.normalization as norm
+import src.tokenization as token
+import src.stemming as lem
+
+
+def main():
+    norm.main()
+    token.main()
+    lem.main()
+
+
+if __name__ == "__main__":
+    main()
