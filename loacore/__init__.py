41d64f03a012a37d57a3553680a25ecf72d8caa5
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-07-16 14:38:49 -0500

    Fix DB_PATH

--- loacore/__init__.py
+++ loacore/__init__.py
@@ -2,4 +2,4 @@ import os
 
 DB_PATH = os.path.abspath(
     os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), '..', '..', 'data', 'database', 'reviews.db'))
+        os.path.abspath(os.path.dirname(__file__)), '..', 'data', 'database', 'reviews.db'))
