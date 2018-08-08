d4a4217f60ee2d050f1d841157a4f20759e0d6e3
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-08 17:02:56 -0500

    Validation functions

---
+++ loacore/learning/validation.py
@@ -0,0 +1,26 @@
+def k_fold_validation(reviews, wv, k):
+    from sklearn.model_selection import cross_val_score
+    import loacore.svm as svm
+    import loacore.learning.word_embeddings as we
+    from sklearn.svm import LinearSVC
+
+    reviews_vectors = we.reviews_2_vec(reviews, wv)
+    labels = svm.get_labels_vector(reviews)
+    clf = LinearSVC()
+
+    print(cross_val_score(clf, reviews_vectors, labels, cv=k))
+
+
+def leave_p_out_validation(reviews, wv, p):
+    from sklearn.model_selection import cross_val_score
+    import loacore.svm as svm
+    import loacore.learning.word_embeddings as we
+    from sklearn.svm import LinearSVC
+    from sklearn.model_selection import LeavePOut
+
+    lpo = LeavePOut(p=p)
+    reviews_vectors = we.reviews_2_vec(reviews, wv)
+    labels = svm.get_labels_vector(reviews)
+    clf = LinearSVC()
+
+    print(cross_val_score(clf, reviews_vectors, labels, cv=lpo))
