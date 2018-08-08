11abb0ca7fa801fb794c3c072686c7ddaf117365
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-08 15:08:23 -0500

    Saving conf to db

--- loacore/__init__.py
+++ loacore/__init__.py
@@ -1,13 +1,33 @@
 import os
 
+# FR_PATH = "/usr/local/share/"
+# lang = "es"
+# LANG_PATH = os.path.abspath(os.path.join(FR_PATH, "freeling", lang))
+
 RESULT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'results'))
 DATA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
 DB_PATH = os.path.abspath(os.path.join(DATA_PATH, 'database', 'reviews.db'))
 RESOURCES_PATHS = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'resources'))
 
-FR_PATH = "/usr/local/share/"
-lang = "es"
-LANG_PATH = os.path.abspath(os.path.join(FR_PATH, "freeling", lang))
+lang = ""
+FR_PATH = ""
+LANG_PATH = ""
+
+
+def _load_conf():
+    import sqlite3 as sql
+    conn = sql.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute("SELECT lang, freeling_path FROM Configuration")
+    result = c.fetchone()
+    global FR_PATH
+    FR_PATH = os.path.join(result[1].split('/'))
+    global lang
+    lang = result[0]
+    global LANG_PATH
+    LANG_PATH = os.path.abspath(os.path.join(FR_PATH, "freeling", lang))
+
+    conn.close()
 
 
 def set_lang(user_lang):
@@ -17,19 +37,38 @@ def set_lang(user_lang):
     :param user_lang: Freeling language
     :type user_lang: str
     """
-    global lang
-    global LANG_PATH
-    lang = user_lang
-    LANG_PATH = FR_PATH + "freeling/" + lang + "/"
+    import sqlite3 as sql
+
+    conn = sql.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute("DELETE FROM Configuration")
+    c.execute("INSERT INTO Configuration (lang, freeling_path) VALUES (?, ?)", (user_lang, FR_PATH))
+
+    conn.close()
+
+    _load_conf()
 
 
 def set_freeling_path(freeling_path):
     """
     Set Freeling path.
-    :param freeling_path: Absolute path of the folder containing the *freeling* folder.
+    :param freeling_path:
+        Absolute path of the folder containing the *freeling* folder.\n
+        The path must always be specified in the following format : */path/to/freeling* .
+        Python will then automatically format it according to the current OS.
     :type freeling_path: :obj:`path-like object`
     """
-    global FR_PATH
-    global LANG_PATH
-    FR_PATH = freeling_path
-    LANG_PATH = os.path.abspath(os.path.join(FR_PATH, "freeling/", lang))
+
+    import sqlite3 as sql
+
+    conn = sql.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute("DELETE FROM Configuration")
+    c.execute("INSERT INTO Configuration (lang, freeling_path) VALUES (?, ?)", (lang, freeling_path))
+
+    conn.close()
+
+    _load_conf()
+
+
+_load_conf()
