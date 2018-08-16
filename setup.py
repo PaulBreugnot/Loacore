b4b7be77e0ab2af9199cb06997f01b1c66772f5c
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-16 15:51:48 -0500

    Setup

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='loacore',
       author="Paul Breugnot",
       author_email='paul.breugnot@etu.emse.fr',
       description="Language and Opinion Analyzer For Comments and Reviews",
-      packages=find_packages(exclude=['test']),
+      packages=find_packages(exclude=['test', 'results', 'resources']),
       long_description=open('README.md').read(),
       setup_requires=['nltk', 'matplotlib', 'PrettyTable'])
