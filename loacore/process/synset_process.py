049d4ef2ad08378f1e1b85ac1521a977b393be56
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-16 19:48:16 -0500

    Corrected api import

--- loacore/process/synset_process.py
+++ loacore/process/synset_process.py
@@ -7,7 +7,7 @@ from loacore.utils.status import ProcessState
 
 import importlib
 from loacore.conf import FREELING_API
-freeling = importlib.import_module("freeling."I + FREELING_API+".pyfreeling")
+freeling = importlib.import_module("freeling." + FREELING_API+".pyfreeling")
 
 def add_synsets_to_sentences(sentences, print_synsets=False,
                              _state_queue=None, _id_process=None, freeling_modules=None):
