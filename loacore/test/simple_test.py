6c1a4684f0c289c1a4478fb2a3967d1fafa61edb
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-22 15:07:45 -0500

    main

--- loacore/test/simple_test.py
+++ loacore/test/simple_test.py
@@ -1,10 +1,16 @@
-import loacore.utils.db as db
-import loacore.process.file_process as file_process
 
-backup = db.database_backup()
-backup.write_backup()
+def main():
+    import loacore.utils.db as db
+    import loacore.process.file_process as file_process
 
-db.download_db(db_name="new")
-file_process.add_files(["test_file.txt"], workers=0)
+    backup = db.database_backup()
+    backup.write_backup()
 
-db.restore_db(backup.backup_file, close=True)
+    db.download_db(db_name="new")
+    file_process.add_files(["test_file.txt"], workers=0)
+
+    db.restore_db(backup.backup_file, close=True)
+
+
+if __name__ == "__main__":
+    main()
