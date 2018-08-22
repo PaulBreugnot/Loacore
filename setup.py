090c35d4dfe3583f99191b8fda3ffb00d338290e
Paul Breugnot <32193009+PaulBreugnot@users.noreply.github.com>
2018-08-22 09:54:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='loacore',
       # packages=find_packages(exclude=['test', 'results', 'resources']),
       packages=find_packages(exclude=['test']),
       # package_dir={'': 'loacore'},
-      package_data={'': ['data', 'docs']},
+      package_data={'': ['data/', 'docs/']},
       long_description=open('README.md').read(),
       install_requires=['PyFreelingApi', 'nltk', 'matplotlib', 'PrettyTable']
      )
