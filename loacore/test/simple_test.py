a5feaac058c80e5824a8a8f99838b7597407d913
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-08-22 15:14:12 -0500

    main workers parameter

--- loacore/test/simple_test.py
+++ loacore/test/simple_test.py
@@ -1,5 +1,5 @@
 
-def main():
+def main(workers=0):
     import os
     import loacore.utils.db as db
     import loacore.process.file_process as file_process
@@ -8,7 +8,7 @@ def main():
     backup.write_backup()
 
     db.download_db(db_name="new")
-    file_process.add_files([os.path.join(os.path.dirname(__file__), "test_file.txt")], workers=0)
+    file_process.add_files([os.path.join(os.path.dirname(__file__), "test_file.txt")], workers=workers)
 
     db.restore_db(backup.backup_file, close=True)
 
